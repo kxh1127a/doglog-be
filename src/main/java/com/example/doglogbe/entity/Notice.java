14b3b11c994535df8c8ddacd6b08fd922c9b5220
leegh <akexz99@gmail.com>
2025-05-13 16:38:20 +0900

    feat: Add Notice Entity

---
+++ src/main/java/com/example/doglogbe/entity/Notice.java
@@ -0,0 +1,28 @@
+package com.example.doglogbe.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class Notice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Boolean isEnable;
+}
