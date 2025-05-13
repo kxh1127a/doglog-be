3186af5e78da9edc10cb4b047e59bb10118eeeac
kxh1127a <kxh1127a@gmail.com>
2025-05-13 12:38:27 +0900

    chore: 초기 프로젝트 세팅 및 application.yml 생성
    - application.yml 기본 설정 추가
    - src 디렉토리 및 작업용 기본 폴더 구조 생성

---
+++ src/test/java/com/example/doglogbe/DoglogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doglogbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoglogBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
