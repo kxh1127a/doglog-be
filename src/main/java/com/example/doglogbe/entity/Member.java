3d3ab7b159a9d3838d63f88bd42600773d79e18e
yoondd <yoondd@kakao.com>
2025-05-14 15:33:04 +0900

    feat: Member entity create - Member 엔티티 생성 - type 폴더 생성 및 MemberRole 역할 enum생성

---
+++ src/main/java/com/example/doglogbe/entity/Member.java
@@ -0,0 +1,51 @@
+package com.example.doglogbe.entity;
+
+import com.example.doglogbe.type.MemberRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MemberRole role;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 255, unique = true)
+    private String userName;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String phone;
+
+
+    @Column(nullable = false, length = 255, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = true)
+    private LocalDateTime lastLoginAt;
+
+    @Column(nullable = false)
+    private Boolean isEnabled;
+
+    @Column(nullable = true, length = 20)
+    private String statusReason;
+
+}
---
+++ src/main/java/com/example/doglogbe/type/MemberRole.java
@@ -0,0 +1,6 @@
+package com.example.doglogbe.type;
+
+public enum MemberRole {
+    ADMIN,
+    USER
+}
