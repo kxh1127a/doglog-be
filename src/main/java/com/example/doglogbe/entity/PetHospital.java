65e8737eabe0be0b8fca08bf63d0811d4ce4880f
yoondd <yoondd@kakao.com>
2025-05-14 15:01:31 +0900

    feat: "PetHospital 테이블 생성"
    - enum타입을 인터넷에서 찾아서 넣었다
    - 추후 확인필요

---
+++ src/main/java/com/example/doglogbe/entity/PetHospital.java
@@ -0,0 +1,50 @@
+package com.example.doglogbe.entity;
+
+import com.example.doglogbe.DoglogBeApplication;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class PetHospital {
+
+    public enum BusinessType {
+        영업,
+        휴업,
+        폐업
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String animalHospitalName;
+
+    @Column(nullable = true, length = 20)
+    private String phoneNumber;
+
+    @Column(nullable = true)
+    private String postAddress;
+
+    @Column(nullable = false)
+    private String roadNameAddress;
+
+    @Column(nullable = true, length = 10)
+    private String careTipNumber;
+
+    @Column(nullable = true)
+    private Double xCoordinate;
+
+    @Column(nullable = true)
+    private Double yCoordinate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BusinessType business;
+
+    @Column(nullable = false)
+    private Boolean isEnable;
+}
