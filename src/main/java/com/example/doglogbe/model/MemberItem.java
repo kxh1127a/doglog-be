61c24afe0556f840d469b1dbd052b1bc084cd43e
kxh1127a <kxh1127a@gmail.com>
2025-05-17 20:02:13 +0900

    the middle of working

---
+++ src/main/java/com/example/doglogbe/model/MemberItem.java
@@ -0,0 +1,25 @@
+package com.example.doglogbe.model;
+
+import com.example.doglogbe.type.MemberRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class MemberItem {
+    private String name;
+    private String userName;
+    private String email;
+    private String phone;
+    private String petName;
+    private String petBirthDate;
+    private LocalDate createdAt;
+    private String lastLoginInfo;
+    private Boolean isEnabled;
+    private String petProfileImageUrl;
+
+}
