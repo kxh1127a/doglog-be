1c77904206ceaa3849936cdba7557f867b3df301
kxh1127a <kxh1127a@gmail.com>
2025-06-09 21:01:51 +0900

    test

--- src/main/java/com/example/doglogbe/controller/MemberController.java
+++ src/main/java/com/example/doglogbe/controller/MemberController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/member")
+@RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
     @PostMapping("/new")
--- src/main/java/com/example/doglogbe/controller/PetBreedController.java
+++ src/main/java/com/example/doglogbe/controller/PetBreedController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/pet-breed")
+@RequestMapping("/pet-breed")
 public class PetBreedController {
     private final PetBreedService petBreedService;
 
