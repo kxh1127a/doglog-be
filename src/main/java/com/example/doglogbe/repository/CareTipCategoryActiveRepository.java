eed0ce991b5df6bc9c369bdc22583eca0dedc96b
yoon <yoondd@kakao.com>
2025-06-15 08:57:59 +0900

    repository 생성

---
+++ src/main/java/com/example/doglogbe/repository/CareTipCategoryActiveRepository.java
@@ -0,0 +1,7 @@
+package com.example.doglogbe.repository;
+
+import com.example.doglogbe.entity.CareTipCategoryActive;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CareTipCategoryActiveRepository extends JpaRepository<CareTipCategoryActive, Long> {
+}
