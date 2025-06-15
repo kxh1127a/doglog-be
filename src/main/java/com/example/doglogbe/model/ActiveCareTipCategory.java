69c0777f77c19de27ee8618a4b246d7c48db405e
yoon <yoondd@kakao.com>
2025-06-15 10:00:07 +0900

    feat: category category api 수정

--- src/main/java/com/example/doglogbe/model/ActiveCareTipCategory.java
+++ src/main/java/com/example/doglogbe/model/ActiveCareTipCategory.java
@@ -8,16 +8,20 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ActiveCareTipCategory {
     private String categoryName;
+    private String category;
 
     private ActiveCareTipCategory(Builder builder){
         this.categoryName = builder.categoryName;
+        this.category = builder.category;
     }
 
     public static class Builder implements CommonModelBuilder<ActiveCareTipCategory>{
         private final String categoryName;
+        private final String category;
 
         public Builder(CareTipCategoryActive careTipCategoryActive){
             this.categoryName = careTipCategoryActive.getCareTipCategory().getName();
+            this.category = careTipCategoryActive.getCareTipCategory().toString();
         }
 
         @Override
