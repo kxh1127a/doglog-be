57f9f4f0451c1a09de7103664f133b4fb4fa2381
kxh1127a <kxh1127a@gmail.com>
2025-06-27 17:07:30 +0900

    feat: SecurityConfige & CustomAccessDeniedHandler 생성

---
+++ src/main/java/com/example/doglogbe/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.example.doglogbe.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+@AllArgsConstructor
+public class SecurityConfig {
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtUtil jwtUtil;
+    private final CustomAccessDe
+}
